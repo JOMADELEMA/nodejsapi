f6fad2f39a7abbface5b9dda4e3cdd1243da901e
JOMADELEMA <jomadelema@gmail.com>
2021-07-26 23:16:13 -0600

    prueba de nodemon

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res)=>{
+app.use("/test", (req, res) => {
     console.log("received request");
-    res.status(200).send("Success"); //la respuesta al navegador
+    res.status(200).send("Success 2"); //la respuesta al navegador
 });
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log("I am ready to listen you");
 });
 
