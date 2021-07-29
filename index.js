d02bb4242ac885068cbb2abad3132dab40d2d5d2
JOMADELEMA <jomadelema@gmail.com>
2021-07-28 21:35:55 -0600

    se agregó la documentación para swagger del api de register user

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express"); //importación de Express
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express(); //creación de la app express
 
@@ -12,6 +14,26 @@ const usersRoutes = require("./routes/users.route");
 //para no usar bodyparser
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// Swagger initialization - Start 
+const swaggerOption = {
+    swaggerDefinition: (swaggerJsdoc.Options = {
+        info: {
+            title: "my-posts",
+            description: "API Documentation",
+            contact: {
+                name: "Developer",
+            },
+            servers: ["http://localhost:3000/"],
+        },
+    }),
+    apis: ["index.js", "./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOption);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); 
+//swagger initialization - End
+
 
 
 //app.use(bodyParser.json());
